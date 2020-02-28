6a41799d772eefb063e00fe22a3185472df90ea4
Deego88 <60344243+Deego88@users.noreply.github.com>
2020-02-28 17:59:57 +0000

    Update weekday.py

--- weekday.py
+++ weekday.py
@@ -1,12 +1,13 @@
-#Ricky Deegan
+#Richard Deegan
 #this program lets you know if its a weekday or not.
-# need to import daytime function.
+#we need to import the datetime function.
 import datetime
 
 #currentday is the variable created to show present day 
 Currentday = datetime.datetime.now( ) 
 #0 = Mon, 1 = Tue, etc. If condition with positional index to identify position of current date in the days of week.
-if Currentday.weekday ( ) < 5:
+#if and elif conditions are used to distingush weekdays from weekends.
+if Currentday.weekday ( ) < 5:  
     print("Yes its a weekday") #print values 0-4
 
 elif Currentday.weekday ( ) > 4:
